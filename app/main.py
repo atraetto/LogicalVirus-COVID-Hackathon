4aef8f5019072ecd87859aeb2db6c34a142af998
Andrew <atraetto@gmail.com>
2020-12-04 09:15:48 -0500

    LogicalVirus Hackathon Front-End

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
 app= Flask(__name__)
 @app.route('/')
 def index():
-  return "<h1>LogicalVirus</h1>"
+  return render_template('index.html')
